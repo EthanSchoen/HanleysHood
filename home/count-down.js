df53108f811761faadf941b94ba46b62cc59e1bf
Derek <derek.schuster@yahoo.com>
2015-08-21 18:54:37 -0400

    finished countdown days

--- home/count-down.js
+++ home/count-down.js
@@ -1,2 +1,21 @@
-//this will be the code to implement a countdown timer on the homepage
-document.getElementById("days-left").innerHTML = "Days left until the AP Exam: " + 140;
+var before = "Valentines day!"
+var current = "Today is Valentines day. Don\'t forget the flowers!"
+var montharray = new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+
+function countdown(yr, m, d) {
+    var today = new Date()
+    var todayy = today.getYear()
+    if (todayy < 1000)
+        todayy += 1900
+    var todaym = today.getMonth()
+    var todayd = today.getDate()
+    var todaystring = montharray[todaym] + " " + todayd + ", " + todayy
+    var futurestring = montharray[m - 1] + " " + d + ", " + yr
+    var difference = (Math.round((Date.parse(futurestring) - Date.parse(todaystring)) / (24*60*60*1000))*1)
+    if (difference == 0)
+        document.getElementById("days-left").innerHTML = current
+    else if (difference > 0)
+        document.getElementById("days-left").innerHTML = "Only "+difference+" days until "+before
+    }
+
+countdown(2016, 5, 3)
